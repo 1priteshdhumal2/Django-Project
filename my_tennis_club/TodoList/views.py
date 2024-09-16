370ca5580063c372ae3882a5aca7d4882e45ddcf
1priteshdhumal2 <priteshsdhumal03@gmail.com>
2024-09-16 22:03:43 +0530

    Created a TodoList using django:

---
+++ my_tennis_club/TodoList/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import Task
+
+# Create your views here.
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request,'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def task_list_view(request):
+    tasks = Task.objects.filter(user= request.user)
+    return render(request, 'task_list.html', {'tasks': tasks})
+    
